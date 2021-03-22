5c227c08d253713cb4d4ae49a33e1137a318eb30
JHall4475 <jhall4475@yaoo.com>
2021-03-22 10:43:31 -0600

    box screen addition

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,12 +2,46 @@ import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
 const BoxScreen = () => {
-    return <View>
-        <Text>Box Screen</Text>
+    return <View style={styles.viewStyle}>
+        <View style={styles.viewOneStyle}></View>
+        <View style={styles.viewTwoStyle}></View>
+        <View style={styles.viewThreeStyle}></View>
+
     </View>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    viewStyle:{
+       flexDirection: 'row',
+       justifyContent: 'space-between'
+      
+    },
+    viewOneStyle:{
+        height:200,
+        width: 300,
+        borderWidth:5,
+        borderColor: 'darkred',
+        backgroundColor: 'red',
+        
+    },
+    viewTwoStyle:{
+        height:200,
+        width: 300,
+        borderWidth:5,
+        borderColor: 'darkgreen',
+        backgroundColor: 'green',
+        marginTop: 200,
+        
+    },
+    viewThreeStyle:{
+        height:200,
+        width: 300,
+        borderWidth:5,
+        borderColor: 'darkblue',
+        backgroundColor: 'blue',
+        
+    },
+})
 
 
 export default BoxScreen;
